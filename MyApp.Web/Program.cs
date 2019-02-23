0e66aa32a1be0b5beaffdbc2b36b53ea22228780
Dennis Moon <dmoon396@hotmail.com>
2019-02-23 16:12:32 -0600

    Added Dockerfile support

--- MyApp.Web/Program.cs
+++ MyApp.Web/Program.cs
@@ -17,33 +17,23 @@ namespace MyApp.Web
               {
                   var env = context.HostingEnvironment;
 
-                  // Call other providers here and call AddKeyPerFile last.
                   config.SetBasePath(env.ContentRootPath)
                       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                       .AddEnvironmentVariables();
 
-                  // Call other providers here and call AddKeyPerFile last.
+                  // Call other providers here and call AddKeyPerFile last
+
                   if (env.IsDevelopment())
                   {
-                      // Use the following config builder method to test local file secrets
-                      // You must create a folder called '/kvmnt' at the root of the web project
-                      // Create a file for each of your secrest, using key name as the secret file name (no file suffix)
-                      // Store the value of the scecret as a single line of text in the secret file
-                      // NOTE: You must comment-out the config.AddUserSecrets<Startup>(); statement above
-
-                      // config.AddKeyPerFile($"{env.ContentRootPath}/kvmnt", false);
-
                       config.AddJsonFile("localsecrets.json", optional: true, reloadOnChange: true);
                   }
                   else
                   {
+                      // The following config builder method reads Azure Key Vault secrets as if they
+                      // were individual files in a folder called '/kvmnt' 
                       config.AddKeyPerFile("/kvmnt", false);
                   }
               })
               .UseStartup<Startup>();
-
-        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>();
     }
 }
